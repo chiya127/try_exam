18492fa4a80b77ca277ffc55f7a132b6f126ef21
chiya127 <chiyazuma@yahoo.co.jp>
2018-06-24 15:22:01 +0900

    回答の取込機能を追加

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,6 +5,10 @@ class Question < ApplicationRecord
   def self.all_update!(questions)
     questions.each do |question|
       self.create!(genre_id: 1, category_name: question['category_name'], title: question['question_title'], detail: question['question_value'] ,question_img_url: nil, consult_name: nil, consult_url: question['consult_url'])
+
+      question['answer_area'].each do |answer|
+        Answer.create!(question_id: self.last.id, title: answer['answer_title'], detail: answer['answer_detail'] , correct_flag: answer['answer_correct_flg'])
+      end
     end
   end
 end
