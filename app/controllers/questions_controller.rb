38e0eda1d89246d35b8efc6566e5d212b6caa4a0
chiya127 <chiyazuma@yahoo.co.jp>
2018-07-08 15:33:14 +0900

    問題をランダムに出力するように対応

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -9,6 +9,7 @@ class QuestionsController < ApplicationController
   RUBY_SILVER_PARAMS = '?private_id=111733705892683188804&quiz_id=49715a2362c000e8c&version=2'
 
   def solution
+    #@questions = @questions.paginate(page: params[:page], per_page: 10, total_entries: limit)
   end
 
   def result
@@ -26,7 +27,8 @@ class QuestionsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions.
   def set_questions
-    @questions = Question.where(genre_id: params[:genre_id]).includes(:answers).all.to_a.take(10)
+    limit = params[:limit] || 50
+    @questions = Question.where(genre_id: params[:genre_id]).includes(:answers).limit(limit).order("RANDOM()")
   end
 
   def get_questions!
