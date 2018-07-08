5a44b3db163711bf755ec0e5a26b94fff8d70200
chiya127 <chiyazuma@yahoo.co.jp>
2018-07-08 16:18:11 +0900

    Ruby Silver 以外の問題投入機能を追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,7 +6,26 @@ class QuestionsController < ApplicationController
 
   BASE_URL = 'https://poropi.net/~webadm/exec/select_question'
 
-  RUBY_SILVER_PARAMS = '?private_id=111733705892683188804&quiz_id=49715a2362c000e8c&version=2'
+  RUBY_SILVER_OPTIONS = {
+    name: 'Ruby Silver',
+    url_params: '?private_id=111733705892683188804&quiz_id=49715a2362c000e8c&version=1'
+  }
+
+  RUBY_GOLD_OPTIONS = {
+    name: 'Ruby Gold',
+    url_params: '?private_id=111733705892683188804&quiz_id=49715a2362c000e8c&version=2'
+  }
+
+  CCNA_OPTIONS = {
+    name: 'CCNA',
+    url_params: '?private_id=116449519389305765867&quiz_id=98145a2bb69470b86&version=1'
+  }
+
+  QUESTION_OPTIONS = [
+    RUBY_SILVER_OPTIONS,
+    RUBY_GOLD_OPTIONS,
+    CCNA_OPTIONS
+  ]
 
   def solution
     #@questions = @questions.paginate(page: params[:page], per_page: 10, total_entries: limit)
@@ -32,15 +51,20 @@ class QuestionsController < ApplicationController
   end
 
   def get_questions!
-    response = open(BASE_URL + RUBY_SILVER_PARAMS)
-    json = JSON.load(response)['result']
-    case json['err_code']
-    when 0
-      Question.all_update!(json['question_data'])
-    when 1
-      @result_message = 'error!!'
-    else
-      @result_message = 'error??'
+    Genre.delete_all
+    Question.delete_all
+    QUESTION_OPTIONS.each do |question_option|
+      response = open(BASE_URL + question_option[:url_params])
+      json = JSON.load(response)['result']
+
+      case json['err_code']
+      when 0
+        Question.all_update!(json['question_data'], question_option[:name])
+      when 1
+        @result_message = 'error!!'
+      else
+        @result_message = 'error??'
+      end
     end
   end
 
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,9 +2,11 @@ class Question < ApplicationRecord
   belongs_to :genre
   has_many :answers
 
-  def self.all_update!(questions)
+  def self.all_update!(questions, genre_name)
+    Genre.create!(name: genre_name)
+    genre_id = Genre.last.id
     questions.each do |question|
-      self.create!(genre_id: 1, category_name: question['category_name'], title: question['question_title'], detail: question['question_value'] ,question_img_url: nil, consult_name: nil, consult_url: question['consult_url'])
+      self.create!(genre_id: genre_id, category_name: question['category_name'], title: question['question_title'], detail: question['question_value'] ,question_img_url: nil, consult_name: nil, consult_url: question['consult_url'])
 
       question['answer_area'].each do |answer|
         Answer.create!(question_id: self.last.id, title: answer['answer_title'], detail: answer['answer_detail'] , correct_flag: answer['answer_correct_flg'])
